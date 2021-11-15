45610b6ad4f62014b10e897eaabb3809c421995e
Jiyun Kim <kimdolsoon@sookmyung.ac.kr>
2021-11-16 01:30:51 +0900

    First commit

---
+++ BOJ_1057.cpp
@@ -0,0 +1,43 @@
+// BOJ_1057 토너먼트
+// [성공]
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int N, J, H;
+
+int ifSameNextRound(int n){
+    if(n%2==1)
+        return (n+1)/2;
+    else
+        return n/2;
+}
+
+void changeNum(){
+    if(J%2==1)
+        J = (J+1)/2;
+    else
+        J = J/2;
+    if(H%2==1)
+        H = (H+1)/2;
+    else
+        H = H/2;
+}
+
+void tournament(){
+    int round = 1;
+    while(true){
+        if((abs(J-H)==1)&&(ifSameNextRound(J)==ifSameNextRound(H))){
+            cout << round;
+            break;
+        }
+        changeNum();
+        round++;
+    }
+}
+
+int main(){
+    cin >> N >> J >> H;
+    tournament();
+}
